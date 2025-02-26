3b7e6834f125556fa20146b5f75c1786b1913234
Luis Fernando Caicedo Solis <125523931+LuisFCaicedo@users.noreply.github.com>
2025-02-26 04:38:27 +0000

    semana 3

---
+++ co/edu/ucc/LuisCaicedo/Semana3/Factory_Method/Vehiculo.java
@@ -0,0 +1,28 @@
+package co.edu.ucc.LuisCaicedo.Semana3.Factory_Method;
+
+interface Vehiculo {
+    void conducir();
+}
+ 
+class CarroElectrico implements Vehiculo {
+    public void conducir() {
+        System.out.println("Conduciendo un carro eléctrico");
+    }
+}
+ 
+class CarroGasolina implements Vehiculo {
+    public void conducir() {
+        System.out.println("Conduciendo un carro a gasolina");
+    }
+}
+ 
+class FabricaVehiculos {
+    public static Vehiculo crearVehiculo(String tipo) {
+        if (tipo.equalsIgnoreCase("electrico")) return new CarroElectrico();
+        else if (tipo.equalsIgnoreCase("gasolina")) return new CarroGasolina();
+        return null;
+    }
+    public void getPatron() {
+        System.out.println("El patrón de diseño utilizado es: Factory Method");
+    }
+}
