ae8ab7f139ae0d29cdc53e5373cb653d1a2ea7cf
Luis Caicedo <lfcaicedo1987@gmail.com>
2025-03-01 01:56:30 -0500

    Historia 2

---
+++ co/edu/ucc/LuisCaicedo/Semana3/Historia2/Main.java
@@ -0,0 +1,13 @@
+package co.edu.ucc.LuisCaicedo.Semana3.Historia2;
+
+import java.util.*;
+import java.util.function.Supplier;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        SystemaLiquidacionPension sistema = new SystemaLiquidacionPension();
+        sistema.ejecutar();
+    }
+}
