5dcee97b5c6c6c2663c82f8af8b49ee81d63ca26
Luis Caicedo <lfcaicedo1987@gmail.com>
2025-04-22 20:00:40 -0500

    ExamenM2

--- co/edu/ucc/LuisCaicedo/ExamenM2/Prototype/Main.java
+++ co/edu/ucc/LuisCaicedo/ExamenM2/Prototype/Main.java
@@ -2,7 +2,7 @@ package ucc.LuisCaicedo.ExamenM2.Prototype;
 
 public class Main {
     public static void main(String[] args) {
-        HistoriaClinica original = new HistoriaClinica("Juan Pérez", "Consulta 20/04");
+        HistoriaClinica original = new HistoriaClinica("Luis Caicedo", "Consulta 20/04");
         HistoriaClinica copia = original.clonar();
         copia.nota = "Consulta 22/04 con receta";
 
